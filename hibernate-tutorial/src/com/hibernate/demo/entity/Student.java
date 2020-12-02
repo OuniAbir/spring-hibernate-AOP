e6579c0d2811e46d8e3f6e40ff793bb88bf1f14f
OuniAbir <ouniabir118@gmail.com>
2020-12-02 14:41:39 +0100

    Hibernate Configuration with Annotations + Creating and Saving Java Objects

---
+++ hibernate-tutorial/src/com/hibernate/demo/entity/Student.java
@@ -0,0 +1,79 @@
+package com.hibernate.demo.entity;
+
+/* Why we are using JPA Annotation instead of Hibernate ?
+ * why we are not using this org.hibernate.annotations.Entity? 
+ * ANSWER:
+ * JPA is a standard specification. Hibernate is an implementation of the JPA specification.
+ * Hibernate implements all of the JPA annotations.
+ * The Hibernate team recommends the use of JPA annotations as a best practice.
+*/
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	//map fields to DB columns 
+	@Id
+	@Column(name="id")
+	private int id;
+
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name ="last_name")
+	private String lastName;
+
+	@Column(name ="email")
+	private String email;
+
+	public Student() {}
+
+	public Student(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		//for debugging 
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	};
+	
+	
+}
