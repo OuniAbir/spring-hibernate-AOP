ae53cd2c465e2c69cefa55a936b0eb3df4304933
OuniAbir <ouniabir118@gmail.com>
2020-12-15 14:25:18 +0100

    AOP: use of PointCut Declaration

--- spring-demo-AOP/src/com/training/aopdemo/aspect/MyDemoLoggingAspecct.java
+++ spring-demo-AOP/src/com/training/aopdemo/aspect/MyDemoLoggingAspecct.java
@@ -2,6 +2,7 @@ package com.training.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,27 +13,52 @@ public class MyDemoLoggingAspecct {
 
 	/*
 	 * will match on the addAccount method in any class
+	 * 
 	 * @Before("execution(public void addAccount())")
 	 */
 
 	/*
 	 * only match on the addAccount method in AccountDAO class
-	 * @Before("execution(public void com.training.aopdemo.dao.AccountDAO.addAccount())")
+	 * 
+	 * @Before("execution(public void com.training.aopdemo.dao.AccountDAO.addAccount())"
+	 * )
 	 */
 
 	/*
 	 * will match on meth starts with "add"
+	 * 
 	 * @Before("execution(public void add*())")
 	 */
 
 	/*
 	 * will match on meth that starts with "add"
+	 * 
 	 * @Before("execution(* add*())")
 	 */
 
-	// will match on any return type for meth that starts with "add"
-	@Before("execution(* add*())")
+	/*
+	 * will match on any return type for meth that starts with "add"
+	 * 
+	 * @Before("execution(* add*())")
+	 */
+
+	/*
+	 * Make use of PointCut Declarations match on any meth on com.training.aopdemo
+	 * package with any params
+	 */
+	
+	@Pointcut("execution(* com.training.aopdemo.dao.*.*(..))")
+	public void forDaoPackage() {
+	};
+
+	@Before("forDaoPackage()")
 	public void beforeaddAccountAdvice() {
 		System.out.println("========> execution @Before advice befor the addAccount meth");
 	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("========>  Perform Api Analytics ");
+	}
+	
 }
